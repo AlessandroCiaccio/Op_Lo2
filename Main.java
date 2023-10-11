00f6103ce32a40784807d65d9002123063b2ea84
Alessandro <sandroscro@libero.it>
2023-10-11 19:46:18 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int x=21;
+        boolean res = (x%2==0);
+        
+        System.out.println("The number "+x+" is even? "+res);
+    }
+}
