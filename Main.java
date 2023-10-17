3b7191994f67e8384c46f9709578067ddc46cd06
Alessandro <sandroscro@libero.it>
2023-10-17 12:13:58 +0200

    Aggiunto metodo

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args){
         int x=21;
-        boolean res = (x%2==0);
         
-        System.out.println("The number "+x+" is even? "+res);
+        System.out.println("The number "+x+" is even? "+evenOdd(x));
+    }
+    public static boolean evenOdd(int x){
+        return x%2==0;
     }
 }
